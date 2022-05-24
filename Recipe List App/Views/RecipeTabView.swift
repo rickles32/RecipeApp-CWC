17d864c366b9b25589d6d4cbf685fa7882a30ffe
Steve Rickley <rickleysteve@gmail.com>
2022-05-24 16:55:33 -0400

    Add tab view

---
+++ Recipe List App/Views/RecipeTabView.swift
@@ -0,0 +1,44 @@
+//
+//  RecipeTabView.swift
+//  Recipe List App
+//
+//  Created by Steve Rickley on 5/24/22.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        
+        TabView {
+            
+            Text("Featured View")
+                .tabItem( {
+                    
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                    
+                })
+            
+            RecipeListView()
+                .tabItem( {
+                    
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                    
+                })
+        }
+        
+        
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
